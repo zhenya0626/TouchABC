00d599158f9f2fa568d92ff44b3ab8b6e040a809
zhenya0626 <riana0626@yahoo.co.jp>
2021-05-15 01:05:23 +0900

    firebase 追加

--- Podfile
+++ Podfile
@@ -4,8 +4,9 @@
 target 'memoryTouchNumber' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
-pod 'RealmSwift'
-pod 'Google-Mobile-Ads-SDK'
+  pod 'RealmSwift'
+  pod 'Google-Mobile-Ads-SDK'
+  pod 'Firebase/Analytics'
   # Pods for memoryTouchNumber
 
   target 'memoryTouchNumberTests' do
--- memoryTouchNumber/AppDelegate.swift
+++ memoryTouchNumber/AppDelegate.swift
@@ -9,7 +9,7 @@
 import UIKit
 import CoreData
 import GoogleMobileAds
-
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +18,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
         GADMobileAds.sharedInstance().start(completionHandler: nil)
         return true
     }
